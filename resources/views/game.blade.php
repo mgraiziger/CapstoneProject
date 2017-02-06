1c4778f93d3a5d8a56ec6ba6d37dd02edd59956a
Matt Graiziger <mgraiziger@msn.com>
2017-02-06 10:33:07 -0600

    First commit/confirming github functionality

---
+++ resources/views/game.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"
+	lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>XHTML 1.0 Strict Template</title>
+	</head>
+	<body>
+		<h3>Game goes here</h3>
+	<body>
+</html>
