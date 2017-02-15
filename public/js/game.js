1e8ed56c26b1cfe4e3e5f5e971fb7d2ae3fad8f1
Matt Graiziger <mgraiziger@msn.com>
2017-02-15 12:53:50 -0600

    Map now works on game.blade.php

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,65 @@
-var wrapper = document.getElementById('wrapper');
+//This script has been migrated to /resources/view/game.blade.php to allow links to work
+
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
+
+var grass = new Image();
+var water = new Image();
+var dirt = new Image();
+
+grass.src = '{{base_path(public/images/grass.jpg)}}';
+water.src = 'water.png';
+dirt.src = 'dirt.png';
+
+
+var xPos = 0;
+var yPos = 0;
+
+var map = [
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,1,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0],
+[0,0,0,1,2,0,0,0,0,0]
+
+];
+
+dirt.onload = function() {
+	for(var i=0; i<map.length; i++)
+	{
+		for(var j=0; j<map[i].length; j++){
+
+		if(map[i][j] == 0) {
+			context.drawImage(grass, xPos, yPos, 50, 50);
+			}
+		
+		if(map[i][j] == 1) {
+			context.drawImage(water, xPos, yPos, 50, 50);
+			}
+
+		if(map[i][j] == 2) {
+			context.drawImage(dirt, xPos, yPos, 50, 50);
+			}
+
+		xPos+=50;
+	
+	
+		}
+		xPos =0;
+		yPos+=50;
+	}
+}
+
+
+
+
+
+
+
+/*var wrapper = document.getElementById('wrapper');
 var newDiv = document.createElement('div');
 
 for (let i = 0; i < 100; i++) {
@@ -14,18 +75,4 @@ for (let i = 0; i < 100; i++) {
 	}
 	let y = '<div class=' + c + ' id =' + i + '></div>';
 	wrapper.innerHTML += y;
-}
-/*    
-    var grass = new Image(10,10);
-    grass.src = '/public/js/grass.jpg';
-    console.log(grass);
-
-grass.onload = function() {
-
-var canvas=document.getElementById(wrapper);
-    var ctx=canvas.getContext("2d");
-    
-    
-    ctx.drawImage(grass,0,0)
-
 }*/
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -8,9 +8,63 @@
 		<link rel="stylesheet" type="text/css" href='{{URL::asset('css/game.css')}}'>
 	</head>
 	<body>
-		<div id="wrapper"></div>
+		<canvas id="canvas" height="500" width="500"></canvas>
 		
+		<script type="text/javascript">
+			var canvas = document.querySelector('#canvas');
+			var context = canvas.getContext('2d');
 
-		<script src='{{URL::asset('js/game.js')}}'></script>
+			var grass = new Image();
+			var water = new Image();
+			var dirt = new Image();
+
+			grass.src = '{{URL::asset('images/grass.png')}}';
+			water.src = '{{URL::asset('images/water.png')}}';
+			dirt.src = '{{URL::asset('images/dirt.png')}}';
+
+
+			var xPos = 0;
+			var yPos = 0;
+
+			var map = [
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,1,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0],
+			[0,0,0,1,2,0,0,0,0,0]
+
+			];
+
+			dirt.onload = function() {
+				for(var i=0; i<map.length; i++)
+				{
+					for(var j=0; j<map[i].length; j++){
+
+					if(map[i][j] == 0) {
+						context.drawImage(grass, xPos, yPos, 50, 50);
+						}
+		
+					if(map[i][j] == 1) {
+						context.drawImage(water, xPos, yPos, 50, 50);
+						}
+
+					if(map[i][j] == 2) {
+						context.drawImage(dirt, xPos, yPos, 50, 50);
+						}
+
+					xPos+=50;
+	
+	
+					}
+					xPos =0;
+					yPos+=50;
+				}
+			}
+		</script>
+
+		<!--<script src='{{URL::asset('js/game.js')}}'></script>-->
 	<body>
 </html>
