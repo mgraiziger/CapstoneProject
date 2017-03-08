508a2a5fedb6a373f803a6c407ff886e306fd792
Unknown <mgraiziger@msn.com>
2017-03-08 16:23:16 -0600

    Changed howteleport() and battleChance() work. Changed how battles work.
    Originally, we were teleporting to another map for our battles (or our battle placeholder). This was causing problems with the teleport function and was generally a pain. Ideally, we don't want the battles to take place in a map, unless we want to do something in real time. I always envisioned a menu system for the battle, and as a result, I've removed the battle map and the teleportation. Now, we have 2 new functions: renderBattle() and endBattle(). renderBattle() draws a placeholder image over the map and sets a boolean that stops the player from moving behind the scenes. This is where we should add code to make the battle do what we want it to do. endBattle resets the boolean to allow the player to move again, and redraws the map. This is what we should call when whatever conditions we want to set are met in the battle. For now, that condition is waiting 2 seconds (with the setTimeout() function).

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -36,17 +36,368 @@
 			var grass = new Image();
 			var water = new Image();
 			var dirt = new Image();
-
+			var battleBackground = new Image();
 			
 			hero.src ='{{URL::asset('images/player.png')}}';
 			portal.src = '{{URL::asset('images/portal.png')}}';
+			battleBackground.src = '{{URL::asset('images/battle.png')}}';
 			grass.src = '{{URL::asset('images/grass.png')}}';
 			water.src = '{{URL::asset('images/water.png')}}';
 			dirt.src = '{{URL::asset('images/dirt.png')}}';
 
 			//0 = grass; 1 = water; 2 = dirt;
 			var map;
-			var maps = {
+			var map1 = [
+			[0,0,0,1,1,0,0,0,0,0],
+			[0,0,0,1,0,0,2,2,0,0],
+			[0,0,0,1,0,0,0,0,0,0],
+			[0,0,0,1,1,1,0,0,0,0],
+			[0,0,0,0,0,1,0,0,0,0],
+			[0,2,0,0,0,1,1,0,0,0],
+			[0,2,0,0,0,0,0,0,0,0],
+			[0,0,1,1,0,0,0,0,0,0],
+			[0,0,1,1,0,0,0,2,2,0],
+			[0,0,0,0,0,0,0,0,0,3]
+			];
+			var map2 = [
+			[2,1,1,1,1,0,0,0,0,0],
+			[2,1,1,1,0,0,2,2,0,0],
+			[2,2,2,1,0,0,0,0,0,0],
+			[0,0,2,1,1,1,0,0,0,0],
+			[0,2,2,0,0,1,0,0,0,0],
+			[0,2,0,0,0,1,1,0,0,0],
+			[0,2,0,0,0,0,0,0,0,0],
+			[0,2,1,1,0,0,2,2,2,2],
+			[0,2,1,1,0,0,2,2,2,2],
+			[0,2,2,2,2,2,2,0,0,3]
+			];
+			var map3 = [
+			[0,0,0,0,0,0,0,1,1,1],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,3]	
+			];
+			var map4 = [
+			[0,0,0,0,0,0,1,1,1,1],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,3]	
+			];
+			var map5 = [
+			[0,0,0,0,0,1,1,1,1,1],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,3]	
+			];
+			/*
+			var enemyMap = [
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[2,0,2,0,2,0,2,0,2,0],
+			[0,1,0,1,0,1,0,1,0,1],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,3]
+			];
+			*/
+
+			//1 = hero; 0 = nothing;
+			var heroMap = [
+			[1,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0]	
+			];
+
+			var loc;
+			var map = map1;
+			var battle = false;
+
+			function renderBattle() {
+				//This redraws the map to the battle image placeholder
+				context.clearRect(0,0,500,500);
+				context.drawImage(battleBackground, 0, 0, 500, 500);
+				//This disallows player movement behind the scenes
+				battle = true;
+				//This is what we have for now instead of a battle. It is a 2 second wait, afterwards we assume the player has 'won' and put them back on the map
+				setTimeout(endBattle, 2000);
+			}
+
+			function endBattle() {
+				battle = false;
+				renderMap();
+			}
+
+			function renderMap() {
+				context.clearRect(0,0,500,500);
+				var xPos = 0;
+				var yPos = 0;
+				//These for loops render the map
+				for(let i=0; i<map.length; i++)
+
+				{
+					for(let j=0; j<map[i].length; j++){
+
+					if(map[i][j] == 0) {
+						context.drawImage(grass, xPos, yPos, 50, 50);
+						}
+		
+					if(map[i][j] == 1) {
+						context.drawImage(water, xPos, yPos, 50, 50);
+						}
+
+					if(map[i][j] == 2) {
+						context.drawImage(dirt, xPos, yPos, 50, 50);
+					}
+
+					if(map[i][j] == 3) {
+						context.drawImage(portal, xPos, yPos, 50, 50);
+					}
+					xPos+=50;
+					}
+					xPos=0;
+					yPos+=50;
+				}
+				renderHero();
+			}
+			
+			
+			function renderHero() {
+				//This renders the hero. It is called in the renderMap() function
+				var xPos= 0;
+				var yPos= 0;
+				for(let i=0; i<heroMap.length; i++)
+				{
+					for(let j=0; j<heroMap[i].length; j++) {
+						if(heroMap[i][j] == 1) {
+							context.drawImage(hero, xPos, yPos, 50, 50);
+						}
+						xPos+=50;
+					}
+					xPos=0;
+					yPos+=50;
+				}
+			}
+
+			function findHero() {
+				//loops through each array within the heroMap array and finds the 1 value representing the hero. It returns [9, -1] if the hero is not found
+				var x = true;
+				var i = -1;
+				do {
+					i++;
+					var charLocation = heroMap[i].indexOf(1);
+					if (i == heroMap.length - 1) 
+						x = false;
+				} while(x && charLocation == -1);
+				return [i, charLocation];
+			}
+
+			function findPortal() {
+				//This finds a portal on the map if there is one. If there is not, it returns [9, -1]
+				var x = true;
+				var i = -1;
+				do {
+					i++;
+					var portalLocation = map[i].indexOf(3);
+					if (i === map.length -1)
+						x = false;
+				} while(x && portalLocation == -1);
+				return [i, portalLocation];
+			}
+			
+
+			function battleChance() {
+				//This uses a switch and a random number to start a battle (currently just a screen redraw, a 2 second wait before you can move again)
+				if (JSON.stringify(findPortal()) != JSON.stringify(findHero())) {
+					heroOverworldLocation = findHero();
+
+					var ran = Math.floor((Math.random() * 10) + 1);
+					switch(ran){
+						case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
+						break;
+						case 9: case 10:
+						renderBattle();
+						break;
+						
+					}
+				}
+			}
+
+			function teleport(key) {
+				//This checks of the player is standing on a portal and if so, teleports them (currently either away from the battle, from map1 to map2 depending on where they are currently). It uses the 'key' variable as a parameter to determine direction.
+				
+				//This if statement should keep a battle from triggering when the player moves to a portal (it would be weird if there were monsters on top of the portal). It does not currently work, probably because it is being called in the wrong order in move() or somewhere else.
+				if (JSON.stringify(findHero()) == JSON.stringify(findPortal())) {
+					heroMap[0][0] = 1;
+					switch(key) {
+						case 37:
+						heroMap[loc[0]][loc[1]-1] = 0;
+						break;
+						case 38:
+						heroMap[loc[0]-1][loc[1]] = 0;
+						break;
+						case 39:
+						heroMap[loc[0]][loc[1]+1] = 0;
+						break;
+						case 40:
+						heroMap[loc[0]+1][loc[1]] = 0;
+						break;
+					}
+					switch(JSON.stringify(map)){
+						case JSON.stringify(map1):
+						map = map2;
+						break;
+						case JSON.stringify(map2):
+						map = map3;
+						break;
+						case JSON.stringify(map3):
+						map = map4;
+						break;
+						case JSON.stringify(map4):
+						map = map5;
+						break;
+						case JSON.stringify(map5):
+						window.alert("You Win!");
+						break;
+					}
+				}
+			}
+
+			function move(event) {
+				var key = event.keyCode;
+				loc = findHero();
+				//the battle variable is a boolean that is true when in a battle. When in a battle, the player cannot move.
+				if (!battle) {
+					//UP
+					if (key == 38) {
+						
+						//First, find the hero on the map/array. findHero() returns a 2 value array where the first value is the players y coordinate, and the second value is the x coordinate (The coordinates are backwards, not for any particular reason besides me not noticing until I was done.)
+
+						//Next, make sure he isn't trying to leave the array (would cause an error), or trying to walk on water
+						//IMPORTANT: The if statement checks the map[] array for water, not the heroMap[] array
+						if (loc[0] > 0 && map[loc[0]-1][loc[1]] != 1) {
+							
+							//if loc = [1,1], the player is attempting to move to [0,1]
+
+							//This sets the value of the array index 'above' the player's current position to be the player (in this case)
+							heroMap[loc[0]-1][loc[1]] = 1;
+
+							//This changes the space the player was on into a 0
+							heroMap[loc[0]][loc[1]] = 0;
+
+							//This checks of the player is standing on a portal and if so, teleports them (currently either away from the battle, from map1 to map2 depending on where they are currently). It uses the key variable as a parameter to determine direction.
+							teleport(key);
+							//Finally, we redraw the map
+							//TODO: Modularize this function if possible
+							renderMap();
+
+							//This uses a random number and a switch to potentially start a battle. For now, the battle is a quick redraw and 2 seconds where the player cannot move.
+							battleChance();
+						}
+					}
+					//DOWN
+					if (key == 40) {
+						if (loc[0] < 9 && map[loc[0]+1][loc[1]] != 1) {
+							heroMap[loc[0]+1][loc[1]] = 1;
+							heroMap[loc[0]][loc[1]] = 0;
+							teleport(key);
+							renderMap();
+							battleChance();
+						}
+					}
+					//LEFT
+					if (key == 37) {
+
+						if (loc[1] > 0 && map[loc[0]][loc[1]-1] != 1) {
+							heroMap[loc[0]][loc[1]-1] = 1;
+							heroMap[loc[0]][loc[1]] = 0;							
+							teleport(key);
+							renderMap();
+							battleChance();
+						}
+					}
+					//RIGHT
+					if (key == 39) {
+						if (loc[1] < 9 && map[loc[0]][loc[1]+1] != 1) {
+							heroMap[loc[0]][loc[1]+1] = 1;
+							heroMap[loc[0]][loc[1]] = 0;
+							teleport(key);
+							renderMap();
+							battleChance();
+						}
+					}
+				}
+			}
+
+			//This should be replaced with a promise if I can figure out how promises work.
+			dirt.onload = function() {
+				renderMap();
+			}
+
+			//Keeps window from scrolling when arrow keys or space bar are pressed
+			document.addEventListener('keydown', function(e) {
+				if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+					e.preventDefault();
+				}
+			}, false);
+
+			//This is what executes move()
+			document.addEventListener('keydown', move);
+	
+		</script>
+
+		<footer id="foot">
+		<strong id="strong"> &#169; Copyright 2017</strong>
+			<p id="enjoy">ENJOY THE GAME!!!</p>
+		</footer>
+		</div>
+		</script>
+	<body>
+</html>
+
+
+
+<!--
+	Temporary Storage
+
+	function sleep(ms) {
+				return new Promise(resolve => setTimeout(resolve, ms));
+			}
+
+			async function demo() {
+				
+				await sleep(2000);
+				
+			}
+
+	var maps = {
 				map1:[
 				[0,0,0,1,1,0,0,0,0,0],
 				[0,0,0,1,0,0,2,2,0,0],
@@ -109,347 +460,4 @@
 				]
 			};
 
-			var map1 = [
-			[0,0,0,1,1,0,0,0,0,0],
-			[0,0,0,1,0,0,2,2,0,0],
-			[0,0,0,1,0,0,0,0,0,0],
-			[0,0,0,1,1,1,0,0,0,0],
-			[0,0,0,0,0,1,0,0,0,0],
-			[0,2,0,0,0,1,1,0,0,0],
-			[0,2,0,0,0,0,0,0,0,0],
-			[0,0,1,1,0,0,0,0,0,0],
-			[0,0,1,1,0,0,0,2,2,0],
-			[0,0,0,0,0,0,0,0,0,3]
-			];
-			var map2 = [
-			[2,1,1,1,1,0,0,0,0,0],
-			[2,1,1,1,0,0,2,2,0,0],
-			[2,2,2,1,0,0,0,0,0,0],
-			[0,0,2,1,1,1,0,0,0,0],
-			[0,2,2,0,0,1,0,0,0,0],
-			[0,2,0,0,0,1,1,0,0,0],
-			[0,2,0,0,0,0,0,0,0,0],
-			[0,2,1,1,0,0,2,2,2,2],
-			[0,2,1,1,0,0,2,2,2,2],
-			[0,2,2,2,2,2,2,0,0,3]
-			];
-			var map3 = [
-			[0,0,0,0,0,0,0,1,1,1],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,3]	
-			];
-			var map4 = [
-			[0,0,0,0,0,0,1,1,1,1],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,3]	
-			];
-			var map5 = [
-			[0,0,0,0,0,1,1,1,1,1],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,3]	
-			];
-			var enemyMap = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[2,0,2,0,2,0,2,0,2,0],
-			[0,1,0,1,0,1,0,1,0,1],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,3]
-			];
-
-			//1 = hero; 0 = nothing;
-			var heroMap = [
-			[1,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0]	
-			];
-
-			var owMap;
-			var heroOverworldLocation;
-			var loc;
-			var map = maps.map1;
-
-			function renderMap() {
-				context.clearRect(0,0,500,500);
-				var xPos = 0;
-				var yPos = 0;
-				//These for loops render the map
-				for(let i=0; i<map.length; i++)
-
-				{
-					for(let j=0; j<map[i].length; j++){
-
-					if(map[i][j] == 0) {
-						context.drawImage(grass, xPos, yPos, 50, 50);
-						}
-		
-					if(map[i][j] == 1) {
-						context.drawImage(water, xPos, yPos, 50, 50);
-						}
-
-					if(map[i][j] == 2) {
-						context.drawImage(dirt, xPos, yPos, 50, 50);
-					}
-
-					if(map[i][j] == 3) {
-						context.drawImage(portal, xPos, yPos, 50, 50);
-					}
-					xPos+=50;
-					}
-					xPos=0;
-					yPos+=50;
-				}
-				renderHero();
-			}
-			
-			
-			function renderHero() {
-				//This renders the hero. It is called in the renderMap() function
-				var xPos= 0;
-				var yPos= 0;
-				for(let i=0; i<heroMap.length; i++)
-				{
-					for(let j=0; j<heroMap[i].length; j++) {
-						if(heroMap[i][j] == 1) {
-							context.drawImage(hero, xPos, yPos, 50, 50);
-						}
-						xPos+=50;
-					}
-					xPos=0;
-					yPos+=50;
-				}
-			}
-
-			function findHero() {
-				//loops through each array within the heroMap array and finds the 1 value representing the hero
-				//Could be replaced with a 2 value array variable which is incremented appropriately during each movement
-				var x = true;
-				var i = -1;
-				do {
-					i++;
-					var charLocation = heroMap[i].indexOf(1);
-					if (i == heroMap.length - 1) 
-						x = false;
-				} while(x && charLocation == -1);
-				return [i, charLocation];
-			}
-
-			function findPortal() {
-				//This finds a portal on the map if there is one. If there is not, it returns [9, -1]
-				var x = true;
-				var i = -1;
-				do {
-					i++;
-					var portalLocation = map[i].indexOf(3);
-					if (i === map.length -1)
-						x = false;
-				} while(x && portalLocation == -1);
-				return [i, portalLocation];
-			}
-
-			function battleChance() {
-				//This uses a switch and a random number to start a battle (currently just a teleport to enemyMap)
-				if (map != enemyMap) {
-					heroOverworldLocation = findHero();
-
-					var ran = Math.floor((Math.random() * 10) + 1);
-					switch(ran){
-						case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
-						break;
-						case 9: case 10:
-						owMap = map;
-						map = enemyMap;
-						break;
-						
-					}
-				}
-			}
-
-			function teleport(key) {
-				//This checks of the player is standing on a portal and if so, teleports them (currently either away from the battle, from map1 to map2 depending on where they are currently). It uses the 'key' variable as a parameter to determine direction.
-				
-				if (JSON.stringify(findHero()) == JSON.stringify(findPortal())) {
-					if (map == enemyMap) {
-						
-						map = owMap;
-						//This line is why the player always goes back to map1 after going to the enemyMap. TODO: Fix that.
-
-						heroMap[heroOverworldLocation[0]][heroOverworldLocation[1]] = 1;
-						switch(key) {
-							case 37:
-							heroMap[loc[0]][loc[1]-1] = 0;
-							break;
-							case 38:
-							heroMap[loc[0]-1][loc[1]] = 0;
-							break;
-							case 39:
-							heroMap[loc[0]][loc[1]+1] = 0;
-							break;
-							case 40:
-							heroMap[loc[0]+1][loc[1]] = 0;
-							break;
-						}
-					} else {
-						if (map = map4) {
-							map = map5;
-						}
-						if (map = map3) {
-							map = map4;
-						}
-						if (map = map2) {
-							map = map3;
-						}
-						if (map = map1) {
-							map = map2;
-						}
-						//map = map2;
-						heroMap[0][0] = 1;
-						switch(key) {
-							case 37:
-							heroMap[loc[0]][loc[1]-1] = 0;
-							break;
-							case 38:
-							heroMap[loc[0]-1][loc[1]] = 0;
-							break;
-							case 39:
-							heroMap[loc[0]][loc[1]+1] = 0;
-							break;
-							case 40:
-							heroMap[loc[0]+1][loc[1]] = 0;
-							break;
-
-						}
-					}
-				}
-			}
-
-
-			function move(event) {
-				var key = event.keyCode;
-				loc = findHero();
-				//UP
-				if (key == 38) {
-					
-					//First, find the hero on the map/array. findHero() returns a 2 value array where the first value is the players y coordinate, and the second value is the x coordinate (The coordinates are backwards, not for any particular reason besides me not noticing until I was done.)
-
-					//Next, make sure he isn't trying to leave the array (would cause an error), or trying to walk on water
-					//IMPORTANT: The if statement checks the map[] array for water, not the heroMap[] array
-					if (loc[0] > 0 && map[loc[0]-1][loc[1]] != 1) {
-						
-
-						//if loc = [1,1], the player is attempting to move to [0,1]
-
-						//This sets the value of the array index 'above' the player's current position to be the player (in this case)
-						heroMap[loc[0]-1][loc[1]] = 1;
-
-						//This changes the space the player was on into a 0
-						heroMap[loc[0]][loc[1]] = 0;
-
-						//This uses a switch and a random number to start a battle (currently just a teleport to enemyMap) 
-						battleChance();
-
-						//This checks of the player is standing on a portal and if so, teleports them (currently either away from the battle, from map1 to map2 depending on where they are currently). It uses the key variable as a parameter to determine direction.
-						teleport(key);
-						//Finally, we redraw the map
-						//TODO: Modularize this function if possible
-						renderMap();
-					}
-				}
-				//DOWN
-				if (key == 40) {
-					if (loc[0] < 9 && map[loc[0]+1][loc[1]] != 1) {
-						heroMap[loc[0]+1][loc[1]] = 1;
-						heroMap[loc[0]][loc[1]] = 0;
-						battleChance();
-						teleport(key);
-						renderMap();
-					}
-				}
-				//LEFT
-				if (key == 37) {
-
-					if (loc[1] > 0 && map[loc[0]][loc[1]-1] != 1) {
-						heroMap[loc[0]][loc[1]-1] = 1;
-						heroMap[loc[0]][loc[1]] = 0;
-						battleChance();
-						teleport(key);
-						renderMap();
-					}
-				}
-				//RIGHT
-				if (key == 39) {
-					if (loc[1] < 9 && map[loc[0]][loc[1]+1] != 1) {
-						heroMap[loc[0]][loc[1]+1] = 1;
-						heroMap[loc[0]][loc[1]] = 0;
-						battleChance();
-						teleport(key);
-						renderMap();
-					}
-				}
-			}
-
-			/*function sleep(ms) {
-				return new Promise(resolve => setTimeout(resolve, ms));
-			}
-
-			async function demo() {
-				
-				await sleep(2000);
-				
-			}*/
-
-			dirt.onload = function() {
-				renderMap();
-			}
-
-			//Keeps window from scrolling when arrow keys or space bar are pressed
-			document.addEventListener('keydown', function(e) {
-				if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-					e.preventDefault();
-				}
-			}, false);
-
-			document.addEventListener('keydown', move);
-	
-		</script>
-
-		<footer id="foot">
-		<strong id="strong"> &#169; Copyright 2017</strong>
-			<p id="enjoy">ENJOY THE GAME!!!</p>
-		</footer>
-		</div>
-		</script>
-	<body>
-</html>
+	-->
